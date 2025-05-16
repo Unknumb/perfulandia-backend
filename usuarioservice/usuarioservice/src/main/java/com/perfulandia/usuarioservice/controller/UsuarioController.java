8f4de86611256cc82ae1d8e80efcdb035d5d7c95
Unknumb <theguzzyboy@gmail.com>
2025-05-16 13:03:06 -0400

    "microservicios usuarios"

---
+++ usuarioservice/usuarioservice/src/main/java/com/perfulandia/usuarioservice/controller/UsuarioController.java
@@ -0,0 +1,43 @@
+package com.perfulandia.usuarioservice.controller;
+import com.perfulandia.usuarioservice.model.Usuario;
+import com.perfulandia.usuarioservice.service.UsuarioService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/usuarios")
+
+public class UsuarioController {
+
+    private final UsuarioService service;
+    //constructor
+    public UsuarioController(UsuarioService service) {
+        this.service = service;
+    }
+
+    //Listar
+    @GetMapping
+    public List<Usuario> listar(){
+        return service.listar();
+    }
+
+    //Post
+    @PostMapping
+    public Usuario guardar(@RequestBody Usuario usuario){
+        return service.guardar(usuario);
+    }
+
+    //Id
+    @GetMapping("/{id}")
+    public Usuario buscar(@PathVariable Long id){
+        return service.buscar(id);
+    }
+
+    //Delete
+    @DeleteMapping("/{id}")
+    public void eliminar(@PathVariable Long id){
+        service.eliminar(id);
+    }
+
+}
--- usuarioservice/usuarioservice/src/main/java/com/perfulandia/usuarioservice/model/Usuario.java
+++ usuarioservice/usuarioservice/src/main/java/com/perfulandia/usuarioservice/model/Usuario.java
@@ -3,17 +3,22 @@ package com.perfulandia.usuarioservice.model;
 import jakarta.persistence.*;
 import lombok.*;
 
+
+
 //Entidad que se va a comportar como una tabla
 @Entity
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
-@Builder ///  Me permite generar constructores flexibles
+@AllArgsConstructor
+@Builder //  Me permite generar constructores flexibles
 public class Usuario {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY);
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
     private String nombre;
     private String correo;
-    private String rol;//ADMIN- GERENTE - USUARIO
-}
+    private String rol;
+
+    }
+
